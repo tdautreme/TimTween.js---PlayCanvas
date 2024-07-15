30011f52f29b6006afd2ec35b577ae1fde944dd4
Timothée DAUTREME <tim.dautreme.pro@gmail.com>
2024-07-16 00:12:25 +0200

    test IIFE

--- src/TimTween.js/TimUtils.js/simulation.js
+++ src/TimTween.js/TimUtils.js/simulation.js
@@ -1,85 +1,85 @@
-var Tim = Tim || {};
+(function(Tim) {
+    Tim.Simulation = function() {
+        // Properties
+        this._timeScale = 1;
+        this._isPlaying = false;
+        this._parentSimulation = undefined;
+        this._deltaTime;
 
-Tim.Simulation = function() {
-    // Properties
-    this._timeScale = 1;
-    this._isPlaying = false;
-    this._parentSimulation = undefined;
-    this._deltaTime;
-
-    // Events
-    this.onUpdate = new Tim.Event(this);
-    this.onPlay = new Tim.Event(this);
-    this.onPause = new Tim.Event(this);
-    this.onIsPlayingChanged = new Tim.Event(this);
-}
-
-Object.defineProperty(Tim.Simulation.prototype, 'deltaTime', {
-    get () {
-        return this._deltaTime;
-    }, set (value) {
-        this._deltaTime = value;
+        // Events
+        this.onUpdate = new Tim.Event(this);
+        this.onPlay = new Tim.Event(this);
+        this.onPause = new Tim.Event(this);
+        this.onIsPlayingChanged = new Tim.Event(this);
     }
-});
 
-Object.defineProperty(Tim.Simulation.prototype, 'timeScale', {
-    get () {
-        return this._timeScale;
-    }, set (value) {
-        this._timeScale = value;
+    Object.defineProperty(Tim.Simulation.prototype, 'deltaTime', {
+        get () {
+            return this._deltaTime;
+        }, set (value) {
+            this._deltaTime = value;
+        }
+    });
+
+    Object.defineProperty(Tim.Simulation.prototype, 'timeScale', {
+        get () {
+            return this._timeScale;
+        }, set (value) {
+            this._timeScale = value;
+        }
+    });
+
+    Object.defineProperty(Tim.Simulation.prototype, 'isPlaying', {
+        get () {
+            return this._isPlaying;
+        }, set (value) {
+            this.setIsPlaying(value);
+        }
+    });
+
+    Object.defineProperty(Tim.Simulation.prototype, 'parentSimulation', {
+        get () {
+            return this._parentSimulation;
+        }, set (value) {
+            if (this.parentSimulation !== undefined && this.isPlaying === true)
+                this.parentSimulation.onUpdate.removeListener(this._update, this);
+            this._parentSimulation = value;
+            if (this.parentSimulation !== undefined && this.isPlaying === true)
+                this.parentSimulation.onUpdate.addListener(this._update, this);
+        }
+    });
+
+    Tim.Simulation.prototype.update = function (deltaTime) {}
+
+    Tim.Simulation.prototype._update = function (deltaTime) {
+        this.deltaTime = deltaTime * this.timeScale;
+        this.update(this.deltaTime);
+        this.onUpdate.trigger(this.deltaTime);
     }
-});
 
-Object.defineProperty(Tim.Simulation.prototype, 'isPlaying', {
-    get () {
-        return this._isPlaying;
-    }, set (value) {
-        this.setIsPlaying(value);
+    Tim.Simulation.prototype.play = function () {
+        if (this.setIsPlaying(true) === false)
+            return;
+        this.onPlay.trigger();
     }
-});
 
-Object.defineProperty(Tim.Simulation.prototype, 'parentSimulation', {
-    get () {
-        return this._parentSimulation;
-    }, set (value) {
-        if (this.parentSimulation !== undefined && this.isPlaying === true)
-            this.parentSimulation.onUpdate.removeListener(this._update, this);
-        this._parentSimulation = value;
-        if (this.parentSimulation !== undefined && this.isPlaying === true)
-            this.parentSimulation.onUpdate.addListener(this._update, this);
+    Tim.Simulation.prototype.pause = function () {
+        if (this.setIsPlaying(false) === false)
+            return;
+        this.onPause.trigger();
     }
-});
 
-Tim.Simulation.prototype.update = function (deltaTime) {}
-
-Tim.Simulation.prototype._update = function (deltaTime) {
-    this.deltaTime = deltaTime * this.timeScale;
-    this.update(this.deltaTime);
-    this.onUpdate.trigger(this.deltaTime);
-}
-
-Tim.Simulation.prototype.play = function () {
-    if (this.setIsPlaying(true) === false)
-        return;
-    this.onPlay.trigger();
-}
-
-Tim.Simulation.prototype.pause = function () {
-    if (this.setIsPlaying(false) === false)
-        return;
-    this.onPause.trigger();
-}
-
-Tim.Simulation.prototype.setIsPlaying = function (isPlaying) {
-    if (this.isPlaying === isPlaying)
-        return false;
-    this._isPlaying = isPlaying;
-    if (this.parentSimulation !== undefined) {
-        if (isPlaying)
-            this.parentSimulation.onUpdate.addListener(this._update, this);
-        else
-            this.parentSimulation.onUpdate.removeListener(this._update, this);
+    Tim.Simulation.prototype.setIsPlaying = function (isPlaying) {
+        if (this.isPlaying === isPlaying)
+            return false;
+        this._isPlaying = isPlaying;
+        if (this.parentSimulation !== undefined) {
+            if (isPlaying)
+                this.parentSimulation.onUpdate.addListener(this._update, this);
+            else
+                this.parentSimulation.onUpdate.removeListener(this._update, this);
+        }
+        this.onIsPlayingChanged.trigger(isPlaying);
+        return true;
     }
-    this.onIsPlayingChanged.trigger(isPlaying);
-    return true;
-}
+})(window.Tim = window.Tim || {});
--- src/TimTween.js/tween-repeat-functions.js
+++ src/TimTween.js/tween-repeat-functions.js
@@ -1,12 +1,13 @@
-var Tim = Tim || {};
-Tim.Tween = Tim.Tween || {};
+(function(Tim) {
+    Tim.Tween = Tim.Tween || {};
 
-Tim.Tween.Repeat = {};
+    Tim.Tween.Repeat = {};
 
-Tim.Tween.Repeat.Restart = function (t) {
-    return Math.moduloUpperBoundInclusive(t, 1);
-}
+    Tim.Tween.Repeat.Restart = function (t) {
+        return Math.moduloUpperBoundInclusive(t, 1);
+    }
 
-Tim.Tween.Repeat.Yoyo = function (t) {
-    return Math.moduloYoyo(t, 1, Math.moduloUpperBoundInclusive);
-}
+    Tim.Tween.Repeat.Yoyo = function (t) {
+        return Math.moduloYoyo(t, 1, Math.moduloUpperBoundInclusive);
+    }
+})(window.Tim = window.Tim || {});
